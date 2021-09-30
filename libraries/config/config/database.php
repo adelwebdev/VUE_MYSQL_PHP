4bcf1011453507af171d89cfccb3ddac712a9421
Adel <83227627+adelwebdev@users.noreply.github.com>
2021-09-30 18:28:10 +0300

    Add files via upload

--- libraries/config/config/database.php
+++ libraries/config/config/database.php
@@ -9,23 +9,32 @@
 
 function getPDO(): PDO
 {
+
     $servername = "localhost";
     $dbname = "vue";
     $user = "root";
-    $pass = " ";
+    $pass = "";
 
     // si on est sur Mac alors $pass="root"
     // une fois que tt ces infos sont stockés sur une variables on fait newPDO (nouvelle instance)
     // on instancie un objet et à la fin on retourne la PDO
     // ca veut dire lorsqu'on appel la fonction getPDO (n'importe où dans notre projet) ça fera une connexion à la db
 
-    $pdo = new PDO("mysql:host=$servername; dbname:=$dbname", $user, $pass, [
-        PDO::ATTR_ERRMODE, PDO::ERRMODE_EXEPTION,
-        // mode de requête par default => revoit des tableaux associatifs
-        PDO::ATR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
-        ]);
+    $pdo = new PDO(
+        "mysql:host=$servername; dbname=$dbname", 
+        $user, 
+        $pass, 
+        [
+        PDO::ATTR_ERRMODE, 
+        PDO::ERRMODE_EXCEPTION,
+        // mode de requête par default => renvoit des tableaux associatifs
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+        ]
+    );
 
         return $pdo;
 }
+
+// attention à bien tt recopier ici !!!!!!!
 
 
