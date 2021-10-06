2cde4dd5fb97b7f4610bbd965019353aaccc8459
Adel <83227627+adelwebdev@users.noreply.github.com>
2021-10-07 00:23:43 +0300

    Add files via upload

--- assets/js/vue.js
+++ assets/js/vue.js
@@ -1,8 +1,24 @@
 const vue = new Vue({
+  data: () => {
+    return {
+      wines: [],
+    };
+  },
+
+  methods: {
+    removeItem(id) {
+      this.$delete(this.wines, id);
+    },
+    getImgUrl(pic) {
+      return "assets/uploads/" + pic;
+    },
+  },
+
   mounted() {
     axios
       .get("libraries/controllers/getData.php")
       .then((res) => res.data)
-      .then((res) => console.log(res));
+      .then((res) => (this.wines = res));
+    // .then((res) => console.log(res));
   },
 }).$mount("#vue-app");
